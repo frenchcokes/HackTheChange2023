c8ab30ee3b24d7a3364809e6211b8d44671c2dcd
WOZARDLOZARD <jerstar36@gmail.com>
2023-11-11 15:57:25 -0700

    Add backend files

--- backend.js
+++ backend.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { join } = require('path');
-const { readFileSync } = require('fs');
 const { post } = require('node-superfetch');
+const { OpenAI } = require('openai');
 require('dotenv').config();
 
-/*
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+});
+
 const app = express();
 app.use(express.static(__dirname));
 
@@ -12,8 +17,11 @@ app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"));
 });
 
+app.post("/analyze", (req, res) => {
+    
+});
+
 app.listen(3000);
-*/
 
 async function detectObjects(image) { // image: /path/to/localImage.png
     const content = Buffer.from(readFileSync(image)).toString("base64");
@@ -28,17 +36,99 @@ async function detectObjects(image) { // image: /path/to/localImage.png
                 },
                 "features": [
                     {
-                        "maxResults": 3,
+                        "maxResults": 5,
                         "type": "OBJECT_LOCALIZATION"
+                    },
+                    {
+                        "maxResults": 5,
+                        "type": "TEXT_DETECTION"
                     },
                 ]
             }
         ]
     });
 
-    if (res && res.body) {
-        console.log(res.body.responses[0].localizedObjectAnnotations);
+    if (res && res.body && res.body.responses.length > 0) {
+        if (!res.body.responses[0].localizedObjectAnnotations || res.body.responses[0].localizedObjectAnnotations.length == 0 || !res.body.responses[0].textAnnotations || res.body.responses[0].textAnnotations.length == 0) {
+            return {
+                objectAnnotations: [],
+                textAnnotations: [],
+            }
+        }
+
+        const objectAnnotations = [];
+        const textAnnotations = [];
+
+        if (res.body.responses[0].localizedObjectAnnotations && res.body.responses[0].localizedObjectAnnotations.length > 0) {
+            var localizedObjectAnnotations = res.body.responses[0].localizedObjectAnnotations.filter(x => x.name.toLowerCase() != "packaged goods" && x.score >= 0.7).filter((x, i, a) => a.map(y => y.name).indexOf(x.name) == i).slice(0, 1);
+
+            console.log(localizedObjectAnnotations.length);
+
+            for (var i = 0; i < localizedObjectAnnotations.length; i++) {
+                const disposal = await openai.chat.completions.create({
+                    messages: [{ role: "user", content: `How do I dispose of ${localizedObjectAnnotations[i].name} in Calgary, Alberta? Provide your answer in one sentence.` }],
+                    model: "gpt-3.5-turbo",
+                });
+
+                if (disposal.choices && disposal.choices[0] && disposal.choices[0].message) {
+                    objectAnnotations.push({
+                        name: localizedObjectAnnotations[i].name,
+                        score: localizedObjectAnnotations[i].score,
+                        bounding: localizedObjectAnnotations[i].boundingPoly.normalizedVertices,
+                        disposal: disposal.choices[0].message.content,
+                    });
+                }
+            }
+        }
+
+        if (res.body.responses[0].textAnnotations && res.body.responses[0].textAnnotations.length > 0) {
+            var normalizedText = res.body.responses[0].textAnnotations[0].description.replace(/\n/g, " ");
+            
+            const product = await openai.chat.completions.create({
+                messages: [{ role: "user", content: `What is the product given by "${normalizedText}"? Answer in up to 3 words, and do not answer in a full sentence.` }],
+                model: "gpt-3.5-turbo",
+            });
+
+            if (product.choices && product.choices[0] && product.choices[0].message) {
+                console.log(product.choices[0].message.content);
+
+                const disposal = await openai.chat.completions.create({
+                    messages: [{ role: "user", content: `How do I dispose of ${product.choices[0].message.content} in Calgary, Alberta? Provide your answer in one sentence.` }],
+                    model: "gpt-3.5-turbo",
+                });
+
+                if (disposal.choices && disposal.choices[0] && disposal.choices[0].message) {
+                    textAnnotations.push({
+                        name: product.choices[0].message.content,
+                        disposal: disposal.choices[0].message.content,
+                    });
+                }
+            }
+        }
+
+        return {
+            objectAnnotations,
+            textAnnotations,
+        }
+    } else {
+        return {
+            objectAnnotations: [],
+            textAnnotations: [],
+        }
     }
+
+    /*
+    [
+        {
+            name
+            score
+            bounding
+            disposal
+        }
+    ]
+    */
 }
 
-detectObjects(join(__dirname, "test.jpeg"));
+async function getDisposal() {
+
+}
